aeda64149c99c9a53ff8428e6d9cce2f18dc232d
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-19 10:33:24 +0200

    mappage ok

--- source/js/index.js
+++ source/js/index.js
@@ -6,14 +6,41 @@ const results = document.getElementById('results');
 let countries;
 let searchTerm = '';
 
-// API REQUEST 
 
+// API REQUEST 
 const fetchCountries = async() => {
 
     countries = await fetch(
         'https://restcountries.eu/rest/v2/all?fields=name;population;flag').then(res => res.json());
 
-        console.log(countries);
-
 };
 
+// Mappage
+const showCountries = async() => {
+
+    await fetchCountries();
+
+    results.innerHTML = (
+
+        countries.filter(country => country.name.toLowerCase().includes(searchTerm.toLowerCase()
+        ))
+        .map(country => (
+
+            `
+                <li class="country-item">
+                    <img class="country-flag" src="${country.flag}" />
+                    <h3 class="country-name">${country.name}</h3>
+                    <div class="country-info">
+                        <h2 class""country-population>${country.population}</h2>
+                        <h5>Habitants</h5>
+                    </div>
+                </li>
+            `
+
+        )).join('')
+
+    );
+};
+
+
+showCountries();
